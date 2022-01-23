2e4ce76e43e2dfa04ec298a768a777ff9b17238b
carretera513 <jon@isonhaus.com>
2022-01-22 22:44:42 -0500

    Added new catch-all route to home

--- udagram/udagram-frontend/src/app/home/home.module.ts
+++ udagram/udagram-frontend/src/app/home/home.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { FeedModule } from '../feed/feed.module';
 import { HomePage } from './homepage/home.page';
 import { HomeRoutingModule } from './home-routing.module';
